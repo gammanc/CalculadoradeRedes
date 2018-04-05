7ee55c909699b28474045aeb3636a17c0497b90e
Kevin Velasquez <00018616@github.com>
2018-04-05 16:34:24 -0600

    Agregado calculo de mascara, wildcard y broadcast con operaciones binarias, y calculo de hosts

--- app/src/main/java/com/velasquez/calculadoraderedes/MainActivity.java
+++ app/src/main/java/com/velasquez/calculadoraderedes/MainActivity.java
@@ -11,11 +11,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit_ip, edit_mask;
-    TextView text_netmask, text_network, text_broadcast, text_test;
+    TextView text_netmask, text_network, text_broadcast, text_hosts;
     Button btn_calcular;
     String network, broadcast;
     int mask[] = new int[4];
-    int wmask[] = new int[4];
+    int wildcard[] = new int[4];
     int mip[] = new int[4];
 
     private final String IP_ADDRESS_PATTERN =
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         text_netmask = findViewById(R.id.text_netmask);
         text_network = findViewById(R.id.text_network);
         text_broadcast = findViewById(R.id.text_broadcast);
-        text_test = findViewById(R.id.text_test);
+        text_hosts = findViewById(R.id.text_hosts);
     }
 
     View.OnClickListener onClick = new View.OnClickListener() {
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 getNetMask();
                 getNetwork();
                 getBroadcast();
+                text_hosts.setText("Hosts: " + (int)Math.pow(2, 32-Integer.parseInt(edit_mask.getText().toString())));
             }
             catch (IllegalArgumentException e) {
                 Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);
@@ -73,28 +74,19 @@ public class MainActivity extends AppCompatActivity {
         String temp = edit_mask.getText().toString().trim();
         if (temp.isEmpty())
             throw new IllegalArgumentException("Campo de máscara vacío");
-        else if (Integer.parseInt(temp) > 32)
+        else if (Integer.parseInt(temp) < 1 || Integer.parseInt(temp) > 32)
             throw new IllegalArgumentException("Valor no permitido para la máscara: " + temp);
         else {
-            String octeto = "";
-            String octetow = "";
-            for (int i = 0; i < 32; i++) {
-                if (i < Integer.parseInt(temp)){
-                    octeto += "1";
-                    octetow += "0";
-                }
-                else {
-                    octeto += "0";
-                    octetow += "1";
-                }
-                if ((i+1) % 8 == 0) {
-                    mask[(i/8)] = Integer.parseInt(octeto, 2);
-                    wmask[(i/8)] = Integer.parseInt(octetow, 2);
-                    octeto = octetow = "";
-                }
-            }
-            text_netmask.setText(getString(R.string.text_netmask) + " "
-                    + mask[0] + "." + mask[1] + "." + mask[2] + "." + mask[3]);
+            int prefix = 0xffffffff << (32 - Integer.parseInt(temp));
+            mask[0] = prefix >>> 24;
+            mask[1] = prefix >> 16 & 0xff;
+            mask[2] = prefix >> 8 & 0xff;
+            mask[3] = prefix & 0xff;
+            wildcard[0] = (~mask[0] << 24) >>> 24;
+            wildcard[1] = (~mask[1] << 24) >>> 24;
+            wildcard[2] = (~mask[2] << 24) >>> 24;
+            wildcard[3] = (~mask[3] << 24) >>> 24;
+            text_netmask.setText("Mascara de Red: "+mask[0]+" . "+mask[1]+" . "+mask[2]+" . "+mask[3]);
         }
     }
 
@@ -105,10 +97,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void getBroadcast() {
-        broadcast = (wmask[0] | mip[0]) + "." + (mip[1] | wmask[1]) + "." + (mip[2] | wmask[2])
-                + "." + (mip[3] | wmask[3]);
+        broadcast = (wildcard[0] | mip[0]) + "." + (mip[1] | wildcard[1]) + "." + (mip[2] | wildcard[2])
+                + "." + (mip[3] | wildcard[3]);
         text_broadcast.setText(getString(R.string.text_broadcast) + " " + broadcast);
 
-        text_test.setText("Wildcard : "+(wmask[0])+" . "+(wmask[1])+" . "+(wmask[2])+" . "+(wmask[3]));
+        text_hosts.setText("Wildcard : "+(wildcard[0])+" . "+(wildcard[1])+" . "+(wildcard[2])+" . "+(wildcard[3]));
     }
 }
